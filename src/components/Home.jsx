6790de72666d29650a525b2448b391a4b43590a8
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-13 13:40:43 +0530

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,44 @@
 import React from 'react'
+import NavigationBar from './NavigationBar'
 
 const Home = () => {
   return (
     <div>
-        <center>
-  <p>
-    <font color="blue">
-      Welcome aboard the Flight Management App!
-    </font>
-    <br /><br />
+      <NavigationBar />
+      <center>
+        <p>
+          <font color="blue">
+            Welcome aboard the Flight Management App!
+          </font>
+          <br /><br />
 
-    <font color="green">
-      Our pilots know left from right, our planes know up from down, and that's already a great start.
-    </font>
-    <br /><br />
+          <font color="green">
+            Our pilots know left from right, our planes know up from down, and that's already a great start.
+          </font>
+          <br /><br />
 
-    <font color="orange">
-      Because every great journey begins with someone saying,
-      <br />
-      "Ladies and Gentlemen, this is your captain speaking..."
-    </font>
-    <br /><br />
+          <font color="orange">
+            Because every great journey begins with someone saying,
+            <br />
+            "Ladies and Gentlemen, this is your captain speaking..."
+          </font>
+          <br /><br />
 
-    <font color="purple">
-      Sit back, relax, fasten your seatbelt, and let the adventure take flight.
-    </font>
-    <br /><br />
+          <font color="purple">
+            Sit back, relax, fasten your seatbelt, and let the adventure take flight.
+          </font>
+          <br /><br />
 
-    <font color="red">
-      Every Flight Has a Story. Make Yours Unforgettable.
-    </font>
-    <br /><br />
+          <font color="red">
+            Every Flight Has a Story. Make Yours Unforgettable.
+          </font>
+          <br /><br />
 
-    <font color="teal">
-      ✈️ Happy Journey! 🌍
-    </font>
-  </p>
-</center>
+          <font color="teal">
+            ✈️ Happy Journey! 🌍
+          </font>
+        </p>
+      </center>
     </div>
   )
 }
