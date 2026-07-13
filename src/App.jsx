56609b5a5f8930e19ccb6a5ef49d8b5dc160ab92
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-13 13:33:13 +0530

    App.jsx Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddFlight from './components/AddFlight'
+import Home from './components/Home'
+import ViewFlight from './components/ViewFlight'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={ <Home/>} />
+          <Route path='/add' element={<AddFlight/> } />
+          <Route path='/view' element={ <ViewFlight />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
     </>
   )
 }
